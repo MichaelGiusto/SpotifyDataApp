9d15604c706a651f2cfc2d6c8e576f801d7760f5
Kaiser Khaja <kkhaja3@gatech.edu>
2024-03-05 15:13:17 -0500

    Added the User class as well as started the SpotifyWrapped class

---
+++ app/src/main/java/com/cs2340/spotifydataapp/SpotifyWrapped.java
@@ -0,0 +1,8 @@
+package com.cs2340.spotifydataapp;
+
+import java.util.ArrayList;
+
+public class SpotifyWrapped {
+    ArrayList<String> topArtists;
+    ArrayList<String> topTracks;
+}
---
+++ app/src/main/java/com/cs2340/spotifydataapp/User.java
@@ -0,0 +1,82 @@
+package com.cs2340.spotifydataapp;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class User {
+
+    String username;
+    String password;
+    ArrayList<String> topArtists;
+    String favoriteAlbum;
+    String favoriteArist;
+    String favoriteShow;
+    Stack<SpotifyWrapped> previousWrapped;
+
+    public User(String username, String password, ArrayList<String> topArtists,
+                String favoriteAlbum, String favoriteArist, String favoriteShow, Stack<SpotifyWrapped> previousWrapped) {
+        this.username = username;
+        this.password = password;
+        this.topArtists = topArtists;
+        this.favoriteAlbum = favoriteAlbum;
+        this.favoriteArist = favoriteArist;
+        this.favoriteShow = favoriteShow;
+        this.previousWrapped = previousWrapped;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<String> getTopArtists() {
+        return topArtists;
+    }
+
+    public void setTopArtists(ArrayList<String> topArtists) {
+        this.topArtists = topArtists;
+    }
+
+    public String getFavoriteAlbum() {
+        return favoriteAlbum;
+    }
+
+    public void setFavoriteAlbum(String favoriteAlbum) {
+        this.favoriteAlbum = favoriteAlbum;
+    }
+
+    public String getFavoriteArist() {
+        return favoriteArist;
+    }
+
+    public void setFavoriteArist(String favoriteArist) {
+        this.favoriteArist = favoriteArist;
+    }
+
+    public String getFavoriteShow() {
+        return favoriteShow;
+    }
+
+    public void setFavoriteShow(String favoriteShow) {
+        this.favoriteShow = favoriteShow;
+    }
+
+    public Stack<SpotifyWrapped> getPreviousWrapped() {
+        return previousWrapped;
+    }
+
+    public void setPreviousWrapped(Stack<SpotifyWrapped> previousWrapped) {
+        this.previousWrapped = previousWrapped;
+    }
+}
