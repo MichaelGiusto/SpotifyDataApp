3e136d416e92f1d7a5b0f9430692912f39e41f15
Kaiser Khaja <kkhaja3@gatech.edu>
2024-03-12 14:49:01 -0400

    Finished up the database helper and created a method to populate our SQLLiteDatabase.

--- app/src/main/java/com/cs2340/spotifydataapp/DatabaseHelper.java
+++ app/src/main/java/com/cs2340/spotifydataapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.cs2340.spotifydataapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -11,6 +12,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // Database version
     private static final int DATABASE_VERSION = 1;
 
+    //table name
+    private static final String TABLE_NAME = "userinfo";
+
+    //name for our track column
+    private static final String TRACKS_COL = "tracks";
+
+    //name for our artist column
+    private static final String ARTISTS_COL = "artists";
+
+
     // Constructor
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,9 +31,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         // Create user table
+        String query = "CREATE TABLE " + TABLE_NAME + " ("
+                + TRACKS_COL + " TEXT, "
+                + ARTISTS_COL + " TEXT)";
         db.execSQL(UserContract.UserEntry.CREATE_TABLE);
     }
 
+    public void addNewUserInfo (String userTracks, String userArtists) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues cv = new ContentValues();
+
+        cv.put(TRACKS_COL, userTracks);
+        cv.put(ARTISTS_COL, userArtists);
+
+        db.insert(TABLE_NAME, null, cv);
+
+        db.close();
+    }
     // Upgrade database
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
