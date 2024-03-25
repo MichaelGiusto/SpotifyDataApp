3ab15ce685f964ef9f1da9a33722b9fb90169876
Kaiser Khaja <kkhaja3@gatech.edu>
2024-03-25 14:01:10 -0400

    added text fields and buttons to the account setting page. Added the delete method to the database helper.

--- app/src/main/java/com/cs2340/spotifydataapp/AccountActivity.java
+++ app/src/main/java/com/cs2340/spotifydataapp/AccountActivity.java
@@ -2,8 +2,10 @@ package com.cs2340.spotifydataapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -13,12 +15,27 @@ import androidx.navigation.ui.NavigationUI;
 
 public class AccountActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
+    private DatabaseHelper dbh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_settings);
+
+        Button updateBtn = findViewById(R.id.update_account_button);
+        Button deleteBtn = findViewById(R.id.delete_account_button);
     }
+
+    void deleteAccount(Button btn) {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //temp value for now
+                dbh.deleteAccount("hi");
+            }
+        });
+    }
+
 
     @Override
     public boolean onSupportNavigateUp() {
